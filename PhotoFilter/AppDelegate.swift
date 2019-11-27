556c4abeee30ce26535aa382a389ab50043d8ffd
Daniel Almeida <danielf.almeida@gmail.com>
2019-11-27 22:43:35 +0000

    Converting NSImage to CIImage to NSImage

--- PhotoFilter/AppDelegate.swift
+++ PhotoFilter/AppDelegate.swift
@@ -17,13 +17,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         let image = NSImage(named: "photo.jpg")
-        self.imageView.image = image
+        
+        let ciImage = CIImageFromNSImage(image!)
+        
+        
+        self.imageView.image = NSImagefromCIImage(ciImage!)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    func CIImageFromNSImage(_ nsImage:NSImage) -> CIImage? {
+        guard let tiffData = nsImage.tiffRepresentation else { return nil }
+        guard let bitmap = NSBitmapImageRep(data:tiffData) else { return nil }
+        
+        return CIImage(bitmapImageRep: bitmap)
+    }
+    
+    /**
+     Creates a NSImage from a CIImage
+     
+     -Parameter ciImage: the input image in CIImage object
+     
+     -Returns: a equivalent NSImage object
+     */
+    func NSImagefromCIImage(_ ciImage:CIImage) -> NSImage? {
+        let rep = NSCIImageRep(ciImage: ciImage)
+        let nsImage =  NSImage(size: rep.size)
+        nsImage.addRepresentation(rep)
+        
+        return nsImage
+    }
 
 }
 
